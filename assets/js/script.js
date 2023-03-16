aafacf62a0f1cbf8e50f34122659ed1b9ae62a93
rbrd87 <rochellembradley@gmail.com>
2023-03-16 10:48:12 +0000

    Created second jumbotron for random quote and used jquery to populate the text/author. Added more css

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,53 @@ function randomQuote() {
         method: "GET",
     }).then(function (randomQuoteData) {
         console.log(randomQuoteData)
-
         searchQuery = randomQuoteData.tags[0]
 
-        console.log(searchQuery)
+        var quoteContent = randomQuoteData.content
+        var quoteAuthor = randomQuoteData.author
+        
+        var randomQuoteEl = $("#quote-content");
+        randomQuoteEl.text(quoteContent)
+
+        var authorEl = $("#author");
+        authorEl.text(quoteAuthor)
 
         // Get random background image
         const unsplashUrl = "https://api.unsplash.com/";
         const apiKey = "uNOwDznJR7H5oeC9LQPuU_EfeLY4DPlkGjR3NrpXOP0";
 
-        $.ajax({
-            url: unsplashUrl + "/search/photos?query=" + searchQuery + "&page=" + pageNo + "&client_id=" + apiKey,
-            method: "GET",
-        }).then(function (pictureData) {
-            console.log(pictureData)
+        // $.ajax({
+        //     url: unsplashUrl + "/search/photos?query=" + searchQuery + "&page=" + pageNo + "&client_id=" + apiKey,
+        //     method: "GET",
+        // }).then(function (pictureData) {
+        //     console.log(pictureData)
 
-            var randomImg = Math.floor(Math.random() * 10);
+        //     var randomImg = Math.floor(Math.random() * 10);
 
-            var backgroundImgEl = $(".custom-jumbotron");
-            backgroundImgEl.css('background-image', 'linear-gradient(0deg, var(--background-color), var(--background-color)), url(' + pictureData.results[randomImg].urls.regular + ')');;
-        });
+        //     var backgroundImgEl = $(".custom-jumbotron");
+        //     backgroundImgEl.css('background-image', 'linear-gradient(0deg, var(--background-color), var(--background-color)), url(' + pictureData.results[randomImg].urls.regular + ')');;
+        // });
     });
 };
 
 randomQuote()
+
+$("#get-random").on('click', function () {
+    var welcomeEl = $("#welcome")
+    var randomQuoteEl = $("#random-quote")
+
+    welcomeEl.addClass("hide")
+    randomQuoteEl.removeClass("hide")
+});
+
+$("#refresh").on("click", function () {
+    randomQuote()
+})
+
+$("#home").on('click', function () {
+    var welcomeEl = $("#welcome")
+    var randomQuoteEl = $("#random-quote")
+    
+    randomQuoteEl.addClass("hide")
+    welcomeEl.removeClass("hide")
+});
