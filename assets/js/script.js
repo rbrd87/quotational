31d231f46787857f469efb8da7c930d8e4d9b69d
rbrd87 <rochellembradley@gmail.com>
2023-03-16 12:55:17 +0000

    Added modal for searching for authors/tags etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,13 @@ function randomQuote() {
         method: "GET",
     }).then(function (randomQuoteData) {
         console.log(randomQuoteData)
-        searchQuery = randomQuoteData.tags[0]
+        console.log(randomQuoteData.tags[0])
+
+        if (randomQuoteData.tags[0] === "famous-quotes") {
+            searchQuery = "nature"
+        } else {
+            searchQuery = randomQuoteData.tags[0]
+        }
 
         var quoteContent = randomQuoteData.content
         var quoteAuthor = randomQuoteData.author
@@ -25,16 +31,18 @@ function randomQuote() {
         // Get random background image
         const unsplashUrl = "https://api.unsplash.com/";
         const apiKey = "uNOwDznJR7H5oeC9LQPuU_EfeLY4DPlkGjR3NrpXOP0";
+        const apiCall = unsplashUrl + "/search/photos?query=" + searchQuery + "&page=" + pageNo + "&client_id=" + apiKey;
 
         // $.ajax({
-        //     url: unsplashUrl + "/search/photos?query=" + searchQuery + "&page=" + pageNo + "&client_id=" + apiKey,
+        //     url: apiCall,
         //     method: "GET",
         // }).then(function (pictureData) {
+        //     console.log(apiCall)
         //     console.log(pictureData)
 
         //     var randomImg = Math.floor(Math.random() * 10);
 
-        //     var backgroundImgEl = $(".custom-jumbotron");
+        //     var backgroundImgEl = $("#random-quote");
         //     backgroundImgEl.css('background-image', 'linear-gradient(0deg, var(--background-color), var(--background-color)), url(' + pictureData.results[randomImg].urls.regular + ')');;
         // });
     });
@@ -60,4 +68,8 @@ $("#home").on('click', function () {
     
     randomQuoteEl.addClass("hide")
     welcomeEl.removeClass("hide")
+});
+
+$("#search").on('click', function () {
+    $('#myModal').modal('show')
 });
